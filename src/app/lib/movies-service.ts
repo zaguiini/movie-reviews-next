e2f45eca2a7e3f3f10fbc0596656ff1f67b149ac
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-06-20 11:46:34 -0300

    Add movies API service

---
+++ src/app/lib/movies-service.ts
@@ -0,0 +1,33 @@
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const authenticatedRequest = (url: string) =>
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+  }).then((response) => response.json());
+
+export interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+  release_date: string;
+}
+
+export const getPopularMovies = (): Promise<{
+  page: number;
+  results: Movie[];
+}> => authenticatedRequest(`${API_BASE_URL}/discover/movie`);
+
+interface QueryMoviesParams {
+  query: string;
+}
+
+export const queryMovies = ({ query }: QueryMoviesParams) => {
+  const url = new URL(`${API_BASE_URL}/search/movie`);
+  url.searchParams.append('query', query);
+
+  return authenticatedRequest(url.toString());
+};
