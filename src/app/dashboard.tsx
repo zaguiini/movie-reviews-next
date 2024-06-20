6224c2af8e6fb7b933bd809ab3b4e55d983d61d7
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-06-19 21:27:47 -0300

    Add base styling with Shadcn

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -4,9 +4,13 @@ export async function Dashboard() {
   const user = await getUser();
 
   return (
-    <div>
-      <h1>Hello, {user.name}!</h1>
-      <a href='/api/auth/logout'>Logout</a>
+    <div className='flex'>
+      <div className='flex flex-1 items-center justify-between'>
+        <h2 className='text-3xl font-bold tracking-tight'>
+          Hello, {user.name}!
+        </h2>
+        <a href='/api/auth/logout'>Logout</a>
+      </div>
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,14 @@
 import { ReactNode } from 'react';
+import { Inter as FontSans } from 'next/font/google';
+import { ThemeProvider } from 'src/components/ThemeProvider';
+
+import './globals.css';
+import { cn } from 'src/lib/utils';
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export default async function RootLayout({
   children,
@@ -6,8 +16,23 @@ export default async function RootLayout({
   children: ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className='flex-1 space-y-4 p-4 pt-6 md:p-8'>{children}</div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
