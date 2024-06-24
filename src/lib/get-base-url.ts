38d887245855756f15595e07837396f363c10d77
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-06-24 15:20:15 -0300

    Fix baseURL function

--- src/lib/get-base-url.ts
+++ src/lib/get-base-url.ts
@@ -1,7 +1,3 @@
 export function getBaseUrl() {
-  return process.env.VERCEL_ENV === 'production'
-    ? process.env.PRODUCTION_URL
-    : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : `http://localhost:${process.env.PORT}`;
+  return process.env.PRODUCTION_URL ?? `http://localhost:${process.env.PORT}`;
 }
